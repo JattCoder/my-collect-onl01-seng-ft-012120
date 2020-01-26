4ff8e67997d7bd2f4553063a3246f73b6b5ef74a
Harmandeep Mand <harmandeepmand.hm@gmail.com>
2020-01-26 04:00:42 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,7 +1,10 @@
-def my_collect (collection)
-  count = 0
-  while count < collection
-    yield(collection[count])
-    count += 1
-  end
+
+def my_collect(array = [])
+i = 0
+new_array = []
+while array[i]
+  new_array << yield(array[i])
+  i += 1
+end
+new_array
 end
