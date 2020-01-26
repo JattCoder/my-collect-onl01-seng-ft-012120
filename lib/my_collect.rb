602aa2e9ac91818f805394f1134a414d4fe23421
Harmandeep Mand <harmandeepmand.hm@gmail.com>
2020-01-26 03:47:23 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,6 +1,8 @@
 def my_collect (collection)
-  collection.each do |name|
-    name = name.split(" ").first
+  count = 0
+  while count < collection.length
+    collection[count] = collection[count].split(" ").delete_at(0)
+    count += 1
   end
   return collection
 end
